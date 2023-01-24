0c43f92e5ac09430a4de86693bdd22433c74e30b
gitak <gitak0127@gmail.com>
2023-01-24 17:55:14 +0900

    jwt 연습

---
+++ src/main/java/com/example/jwttutorial/service/UserService.java
@@ -0,0 +1,63 @@
+package com.example.jwttutorial.service;
+
+import java.util.Collections;
+import java.util.Optional;
+
+import com.example.jwttutorial.dto.UserDto;
+import com.example.jwttutorial.entity.Authority;
+import com.example.jwttutorial.entity.User;
+import com.example.jwttutorial.exception.DuplicateMemberException;
+import com.example.jwttutorial.exception.NotFoundMemberException;
+import com.example.jwttutorial.repository.UserRepository;
+import com.example.jwttutorial.util.SecurityUtil;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class UserService {
+    private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+
+    public UserService(UserRepository userRepository, PasswordEncoder passwordEncoder) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Transactional
+    public UserDto signup(UserDto userDto) {
+        if (userRepository.findOneWithAuthoritiesByUsername(userDto.getUsername()).orElse(null) != null) {
+            throw new DuplicateMemberException("이미 가입되어 있는 유저입니다.");
+        }
+
+        Authority authority = Authority.builder()
+                .authorityName("ROLE_USER")
+                .build();
+
+        User user = User.builder()
+                .username(userDto.getUsername())
+                .password(passwordEncoder.encode(userDto.getPassword()))
+                .nickname(userDto.getNickname())
+                .authorities(Collections.singleton(authority))
+                .activated(true)
+                .build();
+
+        return UserDto.from(userRepository.save(user));
+    }
+
+    @Transactional(readOnly = true)
+    //username을 기준으로 정보를 가져온다.
+    public UserDto getUserWithAuthorities(String username) {
+        return UserDto.from(userRepository.findOneWithAuthoritiesByUsername(username).orElse(null));
+    }
+
+    @Transactional(readOnly = true)
+    //SecurityContext에 저장된 username정보만 가져온다.
+    public UserDto getMyUserWithAuthorities() {
+        return UserDto.from(
+                SecurityUtil.getCurrentUsername()
+                        .flatMap(userRepository::findOneWithAuthoritiesByUsername)
+                        .orElseThrow(() -> new NotFoundMemberException("Member not found"))
+        );
+    }
+}
